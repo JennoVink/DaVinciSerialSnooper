f01a3885db14df69427360937185d50030b0a7e3
JennoVink <jenno-vink@hotmail.com>
2017-09-20 17:34:30 +0200

    Initial commit. The basic of the prototype now works.
    You can set the sensitivity of the sensor (not tested very well due to sensor issues) and it is possible to set a zero-point.

---
+++ GUI.py
@@ -0,0 +1,62 @@
+
+import kivy
+kivy.require('1.0.6') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.slider import Slider
+from kivy.uix.button import Button
+
+from SerialSnooper import DataReader
+
+class MainScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        self.dataReader = DataReader()
+
+        self.cols = 2
+
+        self.button = Button(text='Click here to set the zero-point', font_size=14)
+        self.add_widget(self.button)
+
+        self.enableButton = Button(text='Click here to stop the sensor', font_size=14)
+        self.enableButton.background_color = (255,0,0, .3)
+        self.add_widget(self.enableButton)
+
+        # self.cols = 2
+        sensitivityLabel = Label(text='Sensitivity (now: 10')
+        self.add_widget(sensitivityLabel)
+        self.sensitivitySlider = Slider(value_track=True, value=10, min=0.5, max=80, value_track_color=[1, 0, 0, 1])
+        self.add_widget(self.sensitivitySlider)
+
+        self.add_widget(Label(text='Test Area:'))
+        self.testArea = TextInput(multiline=False)
+        self.add_widget(self.testArea)
+
+        def setZeroPoint(instance):
+            self.dataReader.setAccellOffset(self)
+
+        def toggleSensor(instance):
+            self.dataReader.toggleEnabledButton()
+            self.enableButton.text = 'Click here to stop the sensor' if self.dataReader.enabled else 'Click here to start the sensor'
+            self.enableButton.background_color = (255,0,0, .3) if self.dataReader.enabled else (0, 255, 0, 0.3)
+
+
+        def OnSliderValueChange(instance, value):
+            sensitivityLabel.text = "Sensitivity (now: " + str(value) + ")"
+            self.dataReader.updateSensitivity(value);
+
+        self.sensitivitySlider.bind(value=OnSliderValueChange)
+        self.button.bind(on_press=setZeroPoint)
+        self.enableButton.bind(on_press=toggleSensor)
+
+
+class SerialSnooperApp(App):
+    def build(self):
+        return MainScreen()
+
+if __name__ == '__main__':
+    SerialSnooperApp().run()
---
+++ SerialSnooper.py
@@ -0,0 +1,124 @@
+import serial
+import serial.tools.list_ports
+
+import ctypes
+
+import win32api
+import win32con
+import time
+import threading
+
+
+class DataReader:
+    # config / callibration variables
+    def __init__(self):
+        self.thrG = 40.0
+        self.thrA = 0.05
+        self.accellOffset = 0.0
+        self.state = -1
+        self.lastInput = None
+        self.enabled = True
+
+        SendInput = ctypes.windll.user32.SendInput
+
+        self.ser = None
+        try:
+            comPort = None
+
+            ports = list(serial.tools.list_ports.comports())
+            if len(ports) == 0:
+                print('There are no com ports available')
+                raise Exception('There are no com ports available')
+
+            if len(ports) == 1:
+                print('1 comport found: ', ports[0])
+                comPort = ports[0].device
+
+            for p in ports:
+                # todo: add to a listbox
+                # print(p.description)
+                pass
+
+            self.ser = serial.Serial(comPort, baudrate=115200, timeout=0.01)
+        except Exception:
+            print('Something is wrong with the COM-port.')
+            return
+
+        t = threading.Thread(target=self.startLoop)
+        t.start()
+
+    def setAccellOffset(self, value):
+        if self.lastInput == None:
+            print('There is no input generated at all by the sensor... Exiting')
+            return
+
+        gForceZ = 0.0
+        rotY = 0.0
+        try:
+            gForceZ = float(self.lastInput[0])
+            rotY = float(self.lastInput[1][:-3])
+        except:
+            return
+
+        self.accellOffset = rotY
+        print(self.accellOffset);
+
+    def updateState(self):
+        if not self.enabled:
+            return
+
+        data = self.ser.readline().decode('ascii').split('X')
+        if len(data) == 1:
+            return
+
+        print(data)
+        self.lastInput = data
+
+        gForceZ = 0.0
+        rotY = 0.0
+        try:
+            gForceZ = float(data[0])
+            rotY = float(data[1][:-3])
+        except:
+            return
+
+        if rotY > self.thrG and gForceZ < -self.thrA and self.state != 0:
+            self.state = 0
+            print("Q")
+
+
+        if rotY < - self.thrG and gForceZ > self.thrA and self.state != 1:
+            self.state = 1
+            print("E")
+
+
+        if abs(gForceZ - self.accellOffset) < self.thrA and self.state != 2:
+            self.state = 2
+            win32api.keybd_event(0x25, 0, win32con.KEYEVENTF_KEYUP, 0)
+            win32api.keybd_event(0x27, 0, win32con.KEYEVENTF_KEYUP, 0)
+            print("N")
+
+
+    def startLoop(self):
+        print('hello from startloop :)')
+        time.sleep(3)
+
+        while 1:
+            if self.state is 0:
+                win32api.keybd_event(0x25, 0, 0, 0)
+                # win32api.keybd_event(0x25, 0, win32con.KEYEVENTF_KEYUP, 0)
+            if self.state is 1:
+                win32api.keybd_event(0x27, 0, 0, 0)
+                # win32api.keybd_event(0x27, 0, win32con.KEYEVENTF_KEYUP, 0)
+            self.updateState()
+
+    def updateSensitivity(self, value):
+        self.thrG = value
+
+    def toggleEnabledButton(self):
+        self.enabled = not self.enabled
+        win32api.keybd_event(0x25, 0, win32con.KEYEVENTF_KEYUP, 0)
+        win32api.keybd_event(0x27, 0, win32con.KEYEVENTF_KEYUP, 0)
+        print(self.enabled)
+
+
